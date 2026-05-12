ad074f6b3ab375dc7d22543b38f4c24d64acc072
khansaasasabilaaa-eng <khansaasasabilaaa@gmail.com>
2026-05-12 18:01:35 +0700

    menambahkan class ibu

--- Ibu.h
+++ Ibu.h
@@ -1,4 +1,20 @@
 #ifndef IBU_H
 #define IBU_H
 #include <vector>
+#include <iostream>
+using namespce std;
 
+class ibu {
+public:
+    string nama;
+    vector<anak*> daftar_anak;
+
+    ibu(string pNama) :nama(pNama) {
+        cout << "Ibu \"" << nama << "\" ada\n";
+    }
+    ~ibu() {
+        cout << "Ibu \"" << nama << "\" ada\n";
+    }
+    void tambahAnak(anak*);
+    void cetakAnak();
+};
